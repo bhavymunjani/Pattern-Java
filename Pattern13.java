e906709705a309a983b72550bed8cbd9aea88180
Bhavy Munjani <104751645+bhavymunjani@users.noreply.github.com>
2023-08-07 18:36:11 +0530

    Add files via upload

---
+++ Pattern13.java
@@ -0,0 +1,38 @@
+/*
+       1
+      232
+	 34543
+	4567654
+*/
+
+class Pattern13
+{
+	public static void main(String args[])
+	{
+		int k=0;
+		for(int i=1;i<=4;i++)
+		{
+			k=i;
+			for(int j=1;j<=7;j++)
+			{
+				if(j>=5-i&&j<=3+i)
+				{
+					System.out.print(k);
+					if(j<4)
+					{
+						k++;
+					}
+					else
+					{
+						k--;
+					}
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ Pattern14.java
@@ -0,0 +1,32 @@
+/*
+6 5 4 3 2 1 0
+5 4 3 2 1 0
+4 3 2 1 0
+3 2 1 0
+2 1 0
+1 0
+0
+*/
+
+class Pattern14
+{
+	public static void main(String args[])
+	{	
+		int k;
+		for(int i=1;i<=7;i++)        
+		{
+			k=7-i;
+			for(int j=1;j<=7;j++)   
+			{
+				if(j<=8-i)
+				{
+				System.out.print(k+" ");
+				k--;
+				}
+				else
+				System.out.print(" ");
+			}
+			System.out.println();  
+		}
+	}
+}
