967a60557ab3c4438df07be2f8ab0c70284d993d
Bhavy Munjani <bhavymunjani1418@gmail.com>
2023-08-22 17:12:38 +0530

    Add files via upload
    first commit

---
+++ Pattern26.java
@@ -0,0 +1,43 @@
+/* 
+draw a pattern 
+
+   1
+  212
+ 32123
+4321234
+
+*/
+
+class Pattern26
+{
+	public static void main(String args[])
+	{
+		int k;
+		for(int i=1;i<=4;i++)
+		{
+			k=i;
+			for(int j=1;j<=7;j++)
+			{
+				if(j>=5-i&&j<=3+i)
+				{
+					System.out.print(k);
+					if(j<4)
+					{
+						k--;
+					}
+					else
+					{
+						k++;
+					}
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+		
+	}
+}
